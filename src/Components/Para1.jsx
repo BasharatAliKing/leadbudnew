d6dbe20c5dd2e41c6d5043b39732699cd2093857
Basharat Ali <basharatalidogar14@gmail.com>
2024-05-09 16:30:42 +0500

    edit readme and add new.md

--- src/Components/Para1.jsx
+++ src/Components/Para1.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Para1(props) {
   return (
-    <div className='text-sm md:text-md text-para font-normal'>{props.para1}</div>
+    <div className='text-sm md:text-[16px] text-para font-normal'>{props.para1}</div>
   )
 }
