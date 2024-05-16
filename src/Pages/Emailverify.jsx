4b85d0e74fa648dc3052f52aefb59141f082fe99
Basharat Ali <basharatalidogar14@gmail.com>
2024-05-16 16:29:48 +0500

    data added

--- src/Pages/Emailverify.jsx
+++ src/Pages/Emailverify.jsx
@@ -37,6 +37,7 @@ export default function Emailverify() {
   ]
   const [search, setSearch] = useState('');
   const [datashow, setDatashow]=useState(true);
+  const [deliverable,setDeliverable]=useState(true);
   const [num, setNum]=useState(0);
   const handleChange = (e) => {
     setSearch(e.target.value);
@@ -100,10 +101,10 @@ export default function Emailverify() {
                     <div className='font-light'>Male</div></div>
                     <div title='Deliverable' className=' cursor-pointer flex items-center justify-between text-xs mt-1 md:text-[15px]'> 
                     <div className='font-normal'>State</div>
-                    <div className='font-light'>Deliverable</div></div>
+                    <div className='font-light flex items-center gap-1'><div className='h-3 w-3 rounded-full' style={{color: deliverable ?'#56F444':'#FF4646'}}><FaCheckCircle/></div> Deliverable</div></div>
                     <div title={(num<30 && num >= 0)?"Rejected":(num<80 && num>30)?"Risky":"Acceptable"} className=' cursor-pointer flex items-center justify-between text-xs mt-1 md:text-[15px]'> 
                     <div className='font-normal'>Reason</div>
-                    <div className='font-light flex items-center gap-1'> <div className='h-3 w-3 rounded-full text-transparent' style={{backgroundColor:(num>0 && num<30)?'#FF4646':(num>30 && num<80)?'#FFF713':(num>80 && num<=100)?'#56F444':'black'}}>.</div> {(num<30 && num >= 0)?"Rejected":(num<80 && num>30)?"Risky":"Acceptable"}</div></div>
+                    <div className='font-light p-1 rounded-md'  style={{backgroundColor:(num>0 && num<30)?'#FF4646':(num>30 && num<80)?'#FFF713':(num>80 && num<=100)?'#56F444':'#56F444',color:(num>=0 && num<30?'white':'black')}}>  {(num<30 && num >= 0)?"Rejected":(num<80 && num>30)?"Risky":"Acceptable"}</div></div>
                     <div className='flex items-center justify-between text-xs mt-1 md:text-[15px]'> 
                     <div className='font-normal'>Domain</div>
                     <div className='font-light'>xyz.com</div></div>
