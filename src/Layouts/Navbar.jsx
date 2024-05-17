be9743cf687ebc6a6a2473523fcd3ae878d894a8
Basharat Ali <basharatalidogar14@gmail.com>
2024-05-17 13:44:52 +0500

    data added

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -62,8 +62,8 @@ export default function Navbar() {
           {
             open ? null : <div className='flex flex-col absolute left-0 p-3 bg-bgmain w-[100%] top-10  md:hidden'>
               <ul className='flex flex-col gap-3 mx-auto text-center mt-5'>
-                <li className='leadbud-hov flex items-center gap-1 relative '><Link className='font-medium text-sm' to="/b2bdatabase">Why Leadbud</Link> <FaChevronDown />
-                  <ul className='leadbud-submenu bg-white py-3 gap-2 rounded-md top-6 w-full absolute'>
+                <li className='leadbud-hov flex items-center gap-1  '><Link className='font-medium text-sm' to="/b2bdatabase">Why Leadbud</Link> <FaChevronDown className='text-[12px]' />
+                  <ul className='leadbud-submenu bg-white py-3 gap-2 left-0 right-0 rounded-md top-14 mx-auto w-full absolute'>
                     <li><Link onClick={toggleMenu} className='p-2 pr-8 hover:bg-green text-black w-full font-medium' to="/b2bdatabase">B2BDatabase</Link></li>
                     <li><Link onClick={toggleMenu} className='p-2 pr-8 hover:bg-green text-black w-full font-medium' to="/emailfinder">Email-Finder</Link></li>
                     <li><Link onClick={toggleMenu} className='p-2 pr-8 hover:bg-green text-black w-full font-medium' to="/emailverify">Email-Verify</Link></li>
@@ -71,7 +71,7 @@ export default function Navbar() {
                     <li><Link onClick={toggleMenu} className='p-2 pr-8 hover:bg-green text-black w-full font-medium' to="/salesai">SalesAI</Link></li>
                   </ul>
                 </li>
-                <li className='solution'><Link className='font-medium text-sm' to="/">Solutions</Link>
+                <li className='solution flex gap-1 items-center justify-center'><Link className='font-medium text-sm ' to="/">Solutions</Link><FaChevronDown className='text-[12px]' />
               <div className="solution-menu absolute bg-white p-5 sm:px-10 md:px-16 lg:px-20 gap-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 top-[90px] w-full left-0 z-30">
                 {
                   solution.map((val, id) => {
